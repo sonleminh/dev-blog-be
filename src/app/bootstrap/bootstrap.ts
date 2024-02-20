9016833d3fd4c808395e97d390b050560da87132
Son Le <sonlele2000@gmail.com>
2024-02-20 18:27:21 +0700

    cors config

--- src/app/bootstrap/bootstrap.ts
+++ src/app/bootstrap/bootstrap.ts
@@ -21,6 +21,17 @@ export class App {
       AppConfigKey.APP_PREFIX,
     );
     app.use(helmet());
+    app.enableCors({
+      origin: [
+        'https://dev-blog-jpiieu0kp-sonleminh.vercel.app',
+        'http://localhost:8080',
+      ],
+      methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+      credentials: true,
+      allowedHeaders:
+        'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+    });
+
     app.setGlobalPrefix(apiPrefix);
     app.useGlobalPipes(new ValidationPipe(ValidationConfig));
     app.use(
